f5c8c8dfdd32bc749fb5ac95755377423bcd3b5d
Rawat-Arjun <arjunrawat12121@gmail.com>
2024-11-28 14:15:25 +0530

    added new screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:habit_hive/router/go_router.dart';
-import 'package:habit_hive/screens/signin/bloc/signin_bloc.dart';
+import 'package:habit_hive/screens/user_auth_screen/bloc/user_auth_bloc.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screens/onboarding/ui/onboarding_screen.dart
+++ lib/screens/onboarding/ui/onboarding_screen.dart
@@ -1,26 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:habit_hive/screens/onboarding/ui/carousel_onboarding.dart';
+import 'package:habit_hive/screens/user_auth_screen/ui/create_account_screen.dart';
 
-class OnboardingScreen extends StatefulWidget {
+class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
-  State<OnboardingScreen> createState() => _OnboardingScreenState();
-}
-
-class _OnboardingScreenState extends State<OnboardingScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-            onPressed: () {
-              context.go('/signin');
-            },
-            child: const Text('go to signin'))
-      ],
-    ));
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Make your day',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            ),
+            const Text(
+              'valuable and',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            ),
+            const Text(
+              'Vibrant',
+              style: TextStyle(
+                color: Color.fromARGB(255, 208, 171, 58),
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const SizedBox(
+              height: 100,
+              child: Image(
+                image: NetworkImage(
+                  'https://as2.f tcdn.net/v2/jpg/02/05/35/45/1000_F_205354526_8Y8E2VbxvFJccm8T91qLKofTEIDbxgdv.jpg',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            const CarouselOnboarding(),
+            const Text(
+              "LET'S TRACK",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  color: Colors.amber, shape: BoxShape.circle),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const CreateAccountScreen(),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.arrow_forward_ios),
+                color: Colors.black87,
+                iconSize: 70,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
